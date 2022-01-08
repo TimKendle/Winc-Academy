7ae46278e19865dd39ea01b155aa4f23ad4e0289
TimKendle <95182264+TimKendle@users.noreply.github.com>
2022-01-08 15:41:03 +0100

    Add files via upload

---
+++ Second_java/script.js
@@ -0,0 +1,8 @@
+console.log("Hello Winc Academy students")
+// this is a large sum
+/*
+let sum1 = 1230941 * 1823794;
+console.log("1230941 * 1823794 = ", sum1);
+let sum2 = 637263 / 54;
+console.log("637263 / 54 = ", sum2);
+*/
