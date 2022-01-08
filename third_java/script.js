a6b4e072cfcf37e468c93144d94b1d419c4a40a6
TimKendle <95182264+TimKendle@users.noreply.github.com>
2022-01-08 20:15:04 +0100

    Add files via upload

---
+++ third_java/script.js
@@ -0,0 +1,45 @@
+var age = 19;
+
+var isFemale = true;
+
+//driver status
+var driverstatus = true;
+var designatedDriver = false;
+
+
+if (isFemale) {
+    if (age > 17) {
+
+        console.log("you are welcome")
+
+
+    }
+}
+
+
+else {
+
+    console.log("you are NOT welcome")
+
+}
+
+if (age < 18) {
+
+    console.log("you are NOT welcome")
+
+}
+if (driverstatus) {
+    if (designatedDriver) {
+
+        console.log("you are allowed to drive")
+    }
+    else {
+        console.log("you are NOT allowed to drive!")
+    }
+}
+else {
+
+
+    console.log("you are NOT allowed to drive!")
+
+}
