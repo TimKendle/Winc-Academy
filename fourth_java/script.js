06b1dbe6ba71cfd0db48847541fd3d442a93728c
TimKendle <95182264+TimKendle@users.noreply.github.com>
2022-01-09 16:52:33 +0100

    Add files via upload

---
+++ fourth_java/script.js
@@ -0,0 +1,48 @@
+
+
+var myAge = 29;
+
+if (myAge >= 18 && myAge <= 25) {
+
+
+    console.log("you have 50% discount")
+
+} else {
+
+
+    console.log("I am Sorry no discount for you.")
+}
+
+
+var firstName = "Bram"
+
+if (firstName === "Bram" || firstName === "Sarah") {
+
+    console.log("You get a free beer!")
+
+
+} else {
+
+    console.log("sorry no free beer for you..")
+
+}
+
+var totalAmount = 28
+
+if (totalAmount >= 25 && totalAmount < 50) {
+
+    console.log("You get free Bitterballen!")
+
+} else if (totalAmount >= 50 && totalAmount < 100) {
+
+    console.log("You get a free portion of Nachos!")
+
+} else if (totalAmount >= 100) {
+
+    console.log("You get a free bottle of champagne!!")
+
+} else {
+
+    console.log("no present")
+
+}
